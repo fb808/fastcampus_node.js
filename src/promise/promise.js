fd2b3790ab13ceba18991f9dda5ffeeb25db24b3
fb808 <s2019w26@e-mirim.hs.kr>
2021-08-25 15:25:33 +0900

    0825

---
+++ src/promise/promise.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+const { readBuilderProgram } = require('typescript')
+
+/** eslint-disable no-new */
+/** eslint-disable no-console */
+
+function returnPromiseForTimeout() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Math.random())
+    }, 1000)
+  })
+}
+
+returnPromiseForTimeout()
+  .then((value) => {
+    console.log(value)
+    return returnPromiseForTimeout()
+  })
+  .then((value) => {
+    console.log(value)
+    return returnPromiseForTimeout()
+  })
+  .then((value) => {
+    console.log(value)
+    return returnPromiseForTimeout()
+  })
+  .then((value) => {
+    console.log(value)
+    return returnPromiseForTimeout()
+  })
+
+returnPromiseForTimeout()
